361745e63d1aadd4d801368e2c20404b127abfe2
balch <balch61@gmail.com>
2025-04-26 15:14:11 -0700

    removed greeting code

--- composeApp/src/iosMain/kotlin/com/balch/lander/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/balch/lander/MainViewController.kt
@@ -2,4 +2,4 @@ package com.balch.lander
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { LanderApp() }
