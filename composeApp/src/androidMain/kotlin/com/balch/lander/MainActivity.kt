fc30e7792c4fa8683fd077f5969d0da750cd213d
balch <balch61@gmail.com>
2025-04-26 19:20:26 -0700

    Enable edge-to-edge display in MainActivity
    This change ensures the app utilizes the full screen space by enabling edge-to-edge display. It enhances the user experience by removing unnecessary padding around the UI.

--- composeApp/src/androidMain/kotlin/com/balch/lander/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/balch/lander/MainActivity.kt
@@ -3,12 +3,16 @@ package com.balch.lander
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Enable edge-to-edge display
+        enableEdgeToEdge()
 
         setContent {
             LanderApp()
