77158f35577a5328c48c307c50ec608bec2d2cc2
balch <balch61@gmail.com>
2025-05-04 06:49:08 -0700

    updated docs

--- composeApp/src/commonMain/kotlin/com/balch/lander/core/utils/TimeProvider.kt
+++ composeApp/src/commonMain/kotlin/com/balch/lander/core/utils/TimeProvider.kt
@@ -1,5 +1,10 @@
 package com.balch.lander.core.utils
 
+/**
+ * An interface that provides time-related functionalities.
+ * It serves as an abstraction for retrieving the current time and
+ * checking if the reported time is accurate.
+ */
 interface TimeProvider {
 
     fun currentTimeMillis(): Long
